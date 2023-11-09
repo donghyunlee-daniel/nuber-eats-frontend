9ec0df2c2457330101912ce2197ba28201039f12
donghyunlee-daniel <d.lee77668@gmail.com>
2023-11-08 22:39:30 -0500

    Working on Authentication, (17.2 done)

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,50 @@
-import React from "react"
+import React from "react";
+import { isLoggedInVar } from "../apollo";
+import { useForm } from "react-hook-form";
 
-export const LoggedOutRouter = () => <span>Logged Out</span>
+interface IForm {
+  email: string;
+  password: string;
+}
+
+export const LoggedOutRouter = () => {
+  const { register, watch, handleSubmit } = useForm<IForm>();
+  const {
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log("can't create account");
+  };
+
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "This is required",
+              pattern: /^[A-Za-z0-o._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+          
+        </div>
+        <div>
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="password"
+          />
+        </div>
+        <button className="bg-yellow-300 text-white">Submit</button>
+      </form>
+    </div>
+  );
+};
