dc2fca93411446efdc5bb3eacac53e693ba19137
donghyunlee-daniel <d.lee77668@gmail.com>
2023-11-09 21:40:09 -0500

    Set up everything for the mutation part

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,50 +1,19 @@
 import React from "react";
-import { isLoggedInVar } from "../apollo";
-import { useForm } from "react-hook-form";
-
-interface IForm {
-  email: string;
-  password: string;
-}
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Login } from "../pages/login";
+import { CreateAccount } from "../pages/create-account";
 
 export const LoggedOutRouter = () => {
-  const { register, watch, handleSubmit } = useForm<IForm>();
-  const {
-    formState: { errors },
-  } = useForm();
-  const onSubmit = () => {
-    console.log(watch());
-  };
-  const onInvalid = () => {
-    console.log("can't create account");
-  };
-
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-        <div>
-          <input
-            {...register("email", {
-              required: "This is required",
-              pattern: /^[A-Za-z0-o._%+-]+@gmail.com$/,
-            })}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-          
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            required
-            placeholder="password"
-          />
-        </div>
-        <button className="bg-yellow-300 text-white">Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+      <Route path="/create-account" >
+          <CreateAccount/>
+        </Route>
+        <Route path="/">
+          <Login/>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
