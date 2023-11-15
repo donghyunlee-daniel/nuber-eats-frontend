71621cff07fc689bdbd1a1cde6fd7b4b8c7b3adb
donghyunlee-daniel <d.lee77668@gmail.com>
2023-11-14 22:35:33 -0500

    CORS error

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+import { ApolloError, gql, useMutation } from "@apollo/client";
+import { LoginMutation, LoginMutationVariables } from "../gql/graphql";
+
+const LOGIN_MUTATION = gql`
+  mutation login($loginInput: LoginInput!) {
+    login(input: $loginInput) {
+      ok
+      token
+      error
+    }
+  }
+`;
 
 interface ILoginForm {
   email: string;
@@ -13,14 +26,41 @@ export const Login = () => {
     getValues,
     formState: { errors },
   } = useForm<ILoginForm>();
+  const onCompleted = (data: LoginMutation) => {
+    const {
+      login: { error, ok, token },
+    } = data;
+    if (ok) {
+      console.log(token);
+    }
+  };
+  const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
+    LoginMutation,
+    LoginMutationVariables
+  >(LOGIN_MUTATION, {
+    onCompleted: () => null,
+  });
   const onSubmit = () => {
-    
+    if (!loading) {
+      const { email, password } = getValues();
+      loginMutation({
+        variables: {
+          loginInput: {
+            email,
+            password,
+          },
+        },
+      });
+    }
   };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
       <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className="text-3xl text-gray-800">Login</h3>
-        <form onSubmit={handleSubmit(onSubmit)} className="grid gap-3 mt-5 px-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 px-5"
+        >
           <input
             {...register("email", { required: "Email is required" })}
             name="email"
@@ -29,18 +69,32 @@ export const Login = () => {
             placeholder="Email"
             className="input mb-3"
           />
-          {errors.email?.message && (<span className="font-medium text-red-500">{errors.email.message}</span>)}
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
           <input
-            {...register("password", { required: "Password is required", minLength:10 })}
+            {...register("password", {
+              required: "Password is required",
+              //minLength: 10,
+            })}
             name="password"
             required
             type="password"
             placeholder="Password"
             className="input"
           />
-          {errors.password?.message && (<span className="font-medium text-red-500">{errors.password.message}</span>)}
-          {errors.password?.type === "minLength" && (<span className="font-medium text-red-500">Password must be more than 10 chars.</span>)}
-          <button className="btn mt-3">Log In</button>
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          {errors.password?.type === "minLength" && (
+            <FormError errorMessage="Password must be more than 10 chars." />
+          )}
+          <button className="btn mt-3">
+            {loading ? "Loading..." : "Log In"}
+          </button>
+          {loginMutationResult?.login.error && (
+            <FormError errorMessage={loginMutationResult.login.error} />
+          )}
         </form>
       </div>
     </div>
