80c2ac75978a1898b27abd3a740ac4744b1f8392
donghyunlee-daniel <d.lee77668@gmail.com>
2024-10-09 22:03:19 -0400

    Set up is done including tailwind, Apollo and React Router DOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./styles/styles.css";
+import { client } from './apollo';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>
 );
 
