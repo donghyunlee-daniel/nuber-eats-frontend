658a3d1f8490194794b4961ce68a713076be6bf8
donghyunlee-daniel <d.lee77668@gmail.com>
2024-10-10 21:13:20 -0400

    Used react-hook-form in order to use form easily.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { LoggedOutRouter } from './routers/logged-out-router';
+import React from "react";
+import { LoggedOutRouter } from "./routers/logged-out-router";
+import { gql, useQuery, useReactiveVar } from "@apollo/client";
+import { LoggedInRouter } from "./routers/logged-in-router";
+import { isLoggedInVar } from "./apollo";
 
 
 function App() {
-  return (
-    <LoggedOutRouter/>
-  );
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter />;
 }
 
 export default App;
