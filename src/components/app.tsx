685c6fe66add539e512078c8208475a20bcdc257
donghyunlee-daniel <d.lee77668@gmail.com>
2024-11-04 21:58:06 -0500

    Setting up for test

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { LoggedOutRouter } from "./routers/logged-out-router";
+import { LoggedOutRouter } from "../routers/logged-out-router";
 import { gql, useQuery, useReactiveVar } from "@apollo/client";
-import { LoggedInRouter } from "./routers/logged-in-router";
-import { isLoggedInVar } from "./apollo";
+import { LoggedInRouter } from "../routers/logged-in-router";
+import { isLoggedInVar } from "../apollo";
 
 
-function App() {
+export const App = ()  => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   return isLoggedIn ? <LoggedInRouter/> : <LoggedOutRouter />;
 }
 
-export default App;
