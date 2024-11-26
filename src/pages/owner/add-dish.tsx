5aabdd486d0bee3e4b86f1899aaf9a19c7008c0a
donghyunlee-daniel <d.lee77668@gmail.com>
2024-11-25 22:25:32 -0500

    Done with DishOption part from create Dish

--- src/pages/owner/add-dish.tsx
+++ src/pages/owner/add-dish.tsx
@@ -1,5 +1,5 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { useHistory, useParams } from "react-router-dom";
@@ -27,27 +27,38 @@ interface ICreateDishForm {
   name: string;
   price: string;
   description: string;
+  [key: string]: string;
 }
 
 export const AddDish = () => {
-  const {restaurantId} = useParams<IParams>();
+  const { restaurantId } = useParams<IParams>();
   const history = useHistory();
   const [CreateDishMutation, { loading }] = useMutation<
     CreateDishMutation,
     CreateDishMutationVariables
   >(CREATE_DISH, {
-    refetchQueries: [{ query: MY_RESTAURANT, variables:{
-        input:{
-            id: +restaurantId
-        }
-    } }],
+    refetchQueries: [
+      {
+        query: MY_RESTAURANT,
+        variables: {
+          input: {
+            id: +restaurantId,
+          },
+        },
+      },
+    ],
   });
-  const { register, handleSubmit, formState, getValues } =
+  const { register, handleSubmit, formState, getValues, setValue } =
     useForm<ICreateDishForm>({
       mode: "onChange",
     });
   const onSubmit = () => {
-    const { name, price, description } = getValues();
+    const { name, price, description, ...rest } = getValues();
+    const optionObjects = optionsNumber.map((theId) => ({
+      name: rest[`${theId}-optionName`],
+      extra: +rest[`${theId}-optionExtra`],
+    }));
+    console.log(optionObjects);
     CreateDishMutation({
       variables: {
         input: {
@@ -55,12 +66,21 @@ export const AddDish = () => {
           price: +price,
           description,
           restaurantId: +restaurantId,
+          options: optionObjects,
         },
       },
     });
     history.goBack();
   };
-
+  const [optionsNumber, setOptionsNumber] = useState<number[]>([]);
+  const onAddOptionClick = () => {
+    setOptionsNumber((current) => [Date.now(), ...current]);
+  };
+  const onDeleteClick = (idToDelete: number) => {
+    setOptionsNumber((current) => current.filter((id) => id !== idToDelete));
+    setValue(`${idToDelete}-optionName`, "");
+    setValue(`${idToDelete}-optionExtra`, "");
+  };
   return (
     <div className="container flex flex-col items-center mt-20">
       <Helmet>
@@ -93,6 +113,34 @@ export const AddDish = () => {
           type="text"
           placeholder="Description"
         />
+        <div className="my-10">
+          <h4 className="font-medium mb-3 text-lg">Dish Options</h4>
+          <span
+            onClick={onAddOptionClick}
+            className=" cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5"
+          >
+            Add Dish Option
+          </span>
+          {optionsNumber.length !== 0 &&
+            optionsNumber.map((id) => (
+              <div key={id} className="mt-5">
+                <input
+                  {...register(`${id}-optionName`)}
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2 mr-3"
+                  type="text"
+                  placeholder="Option Name"
+                />
+                <input
+                  {...register(`${id}-optionExtra`)}
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2"
+                  type="number"
+                  min={0}
+                  placeholder="Option Extra Price"
+                />
+                <span className=" cursor-pointer text-white bg-red-500 ml-3 py-2 px-4 mt-5 " onClick={() => onDeleteClick(id)}>Delete Option</span>
+              </div>
+            ))}
+        </div>
         <Button
           loading={loading}
           canClick={formState.isValid}
