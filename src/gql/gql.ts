7dffe2cf61dd0a11d25becefaffec6c169fe82d4
donghyunlee-daniel <d.lee77668@gmail.com>
2024-11-26 22:52:51 -0500

    Restaurant Dashboard part is done. Graph is done with using Victory

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -17,6 +17,7 @@ const documents = {
     "\n  fragment RestaurantParts on Restaurant {\n    id\n    name\n    coverImg\n    category {\n      name\n    }\n    address\n    isPromoted\n  }\n": types.RestaurantPartsFragmentDoc,
     "\n  fragment CategoryParts on Category {\n    id\n    name\n    coverImg\n    slug\n    restaurantCount\n  }\n": types.CategoryPartsFragmentDoc,
     "\n  fragment DishParts on Dish {\n    id\n    name\n    price\n    photo\n    description\n    options{\n      name\n      extra\n      choices {\n        name\n        extra\n      }\n    }\n  }\n": types.DishPartsFragmentDoc,
+    "\n  fragment OrderParts on Order{\n  id\n  createdAt\n  total\n  }\n": types.OrderPartsFragmentDoc,
     "\n  query me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n": types.MeDocument,
     "\n  query category($input: CategoryInput!) {\n    category(input: $input) {\n      ok\n      error\n      totalPages\n      totalResults\n      restaurants {\n        ...RestaurantParts\n      }\n      category {\n        ...CategoryParts\n      }\n    }\n  }\n  \n  \n": types.CategoryDocument,
     "\n  query restaurant($input: RestaurantInput!) {\n    restaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n      }\n    }\n  }\n  \n": types.RestaurantDocument,
@@ -24,7 +25,7 @@ const documents = {
     "\n  query searchRestaurant($input: SearchRestaurantInput!) {\n    searchRestaurant(input: $input) {\n      ok\n      error\n      totalPages\n      totalResults\n      restaurants {\n        ...RestaurantParts\n      }\n    }\n  }\n  \n": types.SearchRestaurantDocument,
     "\n  mutation createDish($input: CreateDishInput!) {\n    createDish(input: $input) {\n      ok\n      error\n    }\n  }\n": types.CreateDishDocument,
     "\n  mutation createRestaurant($input: CreateRestaurantInput!) {\n    createRestaurant(input: $input) {\n      ok\n      error\n      restaurantId\n    }\n  }\n": types.CreateRestaurantDocument,
-    "\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu{\n            ...DishParts\n        }\n      }\n    }\n  }\n  \n  \n": types.MyRestaurantDocument,
+    "\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu {\n          ...DishParts\n        }\n        orders {\n        ...OrderParts\n        }\n      }\n    }\n  }\n  \n  \n  \n": types.MyRestaurantDocument,
     "\n  query myRestaurants {\n    myRestaurants {\n      ok\n      error\n      restaurants {\n        ...RestaurantParts\n      }\n    }\n  }\n  \n": types.MyRestaurantsDocument,
     "\n  mutation verifyEmail($input: VerifyEmailInput!) {\n    verifyEmail(input: $input) {\n      ok\n      error\n    }\n  }\n": types.VerifyEmailDocument,
     "\n          fragment VerifiedUser on User {\n            verified\n          }\n        ": types.VerifiedUserFragmentDoc,
@@ -63,6 +64,10 @@ export function graphql(source: "\n  fragment DishParts on Dish {\n    id\n    n
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
+export function graphql(source: "\n  fragment OrderParts on Order{\n  id\n  createdAt\n  total\n  }\n"): (typeof documents)["\n  fragment OrderParts on Order{\n  id\n  createdAt\n  total\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
 export function graphql(source: "\n  query me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n"): (typeof documents)["\n  query me {\n    me {\n      id\n      email\n      role\n      verified\n    }\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -91,7 +96,7 @@ export function graphql(source: "\n  mutation createRestaurant($input: CreateRes
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu{\n            ...DishParts\n        }\n      }\n    }\n  }\n  \n  \n"): (typeof documents)["\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu{\n            ...DishParts\n        }\n      }\n    }\n  }\n  \n  \n"];
+export function graphql(source: "\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu {\n          ...DishParts\n        }\n        orders {\n        ...OrderParts\n        }\n      }\n    }\n  }\n  \n  \n  \n"): (typeof documents)["\n  query myRestaurant($input: MyRestaurantInput!) {\n    myRestaurant(input: $input) {\n      ok\n      error\n      restaurant {\n        ...RestaurantParts\n        menu {\n          ...DishParts\n        }\n        orders {\n        ...OrderParts\n        }\n      }\n    }\n  }\n  \n  \n  \n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,12 +1,25 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
-import { DISH_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
+import {
+  DISH_FRAGMENT,
+  ORDERS_FRAGMENT,
+  RESTAURANT_FRAGMENT,
+} from "../../fragments";
 import {
   MyRestaurantQuery,
   MyRestaurantQueryVariables,
 } from "../../gql/graphql";
 import { Dish } from "../../components/dish";
+import {
+  VictoryAxis,
+  VictoryChart,
+  VictoryLabel,
+  VictoryLine,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from "victory";
 
 export const MY_RESTAURANT = gql`
   query myRestaurant($input: MyRestaurantInput!) {
@@ -15,14 +28,18 @@ export const MY_RESTAURANT = gql`
       error
       restaurant {
         ...RestaurantParts
-        menu{
-            ...DishParts
+        menu {
+          ...DishParts
+        }
+        orders {
+          ...OrderParts
         }
       }
     }
   }
   ${RESTAURANT_FRAGMENT}
   ${DISH_FRAGMENT}
+  ${ORDERS_FRAGMENT}
 `;
 
 interface IParams {
@@ -40,8 +57,8 @@ export const MyRestaurant = () => {
         },
       },
     }
-    );
-    console.log(data);
+  );
+
   return (
     <div>
       <div
@@ -66,10 +83,61 @@ export const MyRestaurant = () => {
         <div className="mt-10">
           {data?.myRestaurant.restaurant?.menu.length === 0 ? (
             <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) :<div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
-            {data?.myRestaurant.restaurant?.menu.map(dish => <Dish name={dish.name} descriptions={dish.description} price={dish.price}/>)}
+          ) : (
+            <div className="grid mt-16 md:grid-cols-3 gap-x-5 gap-y-10">
+              {data?.myRestaurant.restaurant?.menu.map((dish, index) => (
+                <Dish
+                  name={dish.name}
+                  descriptions={dish.description}
+                  price={dish.price}
+                  key={index}
+                />
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="mt-20 mb-10">
+          <h4 className="text-center text-2xl font-medium">Sales</h4>
+          <div className="mt-10">
+            <VictoryChart
+              theme={VictoryTheme.material}
+              height={500}
+              width={window.innerWidth}
+              domainPadding={50}
+              containerComponent={<VictoryVoronoiContainer />}
+            >
+              {" "}
+              <VictoryLine
+                labels={({ datum }) => `$${datum.y}`}
+                labelComponent={
+                  <VictoryTooltip
+                    style={{ fontSize: 17 }}
+                    renderInPortal
+                    dy={-20}
+                  />
+                }
+                style={{
+                  data: {
+                    strokeWidth: 5,
+                    stroke: "4D7C0F",
+                  },
+                }}
+                interpolation="natural"
+                data={data?.myRestaurant.restaurant?.orders.map((order) => ({
+                  x: order.createdAt,
+                  y: order.total,
+                }))}
+              />
+              <VictoryAxis
+              tickLabelComponent={<VictoryLabel renderInPortal/>}
+                style={{
+                  tickLabels: { fontSize: 15, fill: "#4D7C0F", },
+                  
+                }}
+                tickFormat={(tick) => new Date(tick).toLocaleDateString()}
+              />
+            </VictoryChart>
           </div>
-        }
         </div>
       </div>
     </div>
